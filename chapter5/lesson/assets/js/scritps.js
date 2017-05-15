9b9340be53f0b1088b90ceb0978d34eca05fe5fb
Albert Mas <almaslac@gmail.com>
2017-05-15 11:03:45 +0200

    JS native forEach

--- chapter5/lesson/assets/js/scritps.js
+++ chapter5/lesson/assets/js/scritps.js
@@ -18,12 +18,12 @@ var logEach = (arr) => {
 
 // adding an action
 
-function forEach(array, action) {
+function forEach_(array, action) {
   for (var i = 0; i < array.length; i++)
     action(array[i]);
 }
 
-forEach(["Wampeter", "Foma", "Granfalloon"], console.log);
+forEach_(["Wampeter", "Foma", "Granfalloon"], console.log);
 
 var stringsArr = ["Wampeter", "Foma", "Granfalloon"];
 
@@ -32,3 +32,40 @@ const addString = (string) => {
   console.log(`${string} ist lecker`);
 }
 
+// We can also directly pass the function directy as a parameter without storing it in a variable, because JS is awesome
+
+var numbers = [1, 2, 3, 4, 5], sum = 0;
+forEach_(numbers, function(number) {
+  sum += number;
+});
+console.log(sum);
+
+// JS has it's own forEach native function, so we don't need to rewrite it
+
+// it can do functions like 'gatherCorrelations' in ch4 shorter easier to grasp
+
+function gatherCorrelations(journal) {
+  var phis = {};
+  for (var entry = 0; entry < journal.length; entry++) {
+    var events = journal[entry].events;
+    for (var i = 0; i < events.length; i++) {
+      var event = events[i];
+      if (!(event in phis))
+        phis[event] = phi(tableFor(event, journal));
+    }
+  }
+  return phis;
+}
+
+// Using forEach
+
+function gatherCorrelations(journal) {
+  var phis = {};
+  journal.forEach(function(entry) {
+    entry.events.forEach(function(event) {
+      if (!(event in phis))
+        phis[event] = phi(tableFor(event, journal));
+    });
+  });
+  return phis;
+}
