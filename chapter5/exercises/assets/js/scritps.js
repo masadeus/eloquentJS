4c4a839a96072766ccf2b317cab5fb446e29b6b4
Albert Mas <almaslac@gmail.com>
2017-06-14 06:34:13 +0200

    Flattening and JSON parse

--- chapter5/exercises/assets/js/scritps.js
+++ chapter5/exercises/assets/js/scritps.js
@@ -4,22 +4,12 @@
 
 var arrays = [[1, 2, 3], [4, 5], [6]];
 
-function reduce_(array, combine, start) {
-  var current = start;
-  for (var i = 0; i < array.length; i++)
-    current = combine(current, array[i]);
-  return current;
-}
-
 // define empty array to pass as first paramenter
-const arr = [];
+var arrFlat = arrays.reduce(
+  function (a, b) {
+    return a.concat(b);
+});
 
-console.log(reduce_(arrays, function(a, b){
-  return a.concat(b);
-}, arr));
+// MOTHER-CHILD AGE DIFFERENCE
 
-// And now the same using the native method
-
-console.log(arrays.reduce(function(a, b){
-  return a.concat(b);
-}));
+const ancestry = JSON.parse(ANCESTRY_FILE);
